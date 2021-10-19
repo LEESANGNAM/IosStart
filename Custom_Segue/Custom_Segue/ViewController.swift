2946e2fb27a1e3493d0a51a99e9a696e791076b8
LEESANGNAM <fgh0116@naver.com>
2021-10-19 22:51:57 +0900

    Unwind

---
+++ Custom_Segue/Custom_Segue/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Custom_Segue
+//
+//  Created by 이상남 on 2021/10/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBAction func unwindmainVC(segue : UIStoryboardSegue){}
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
