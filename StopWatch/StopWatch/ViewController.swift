45df306c5ab5084d9263f0def9b13556c7285c73
LEESANGNAM <fgh0116@naver.com>
2021-11-08 01:53:18 +0900

    StopWatch_Design

--- StopWatch/StopWatch/ViewController.swift
+++ StopWatch/StopWatch/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelMinute: UILabel!
+    @IBOutlet weak var labelSecond: UILabel!
+    @IBOutlet weak var labelMillisecond: UILabel!
+    
+    @IBOutlet weak var startStopButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    
+    
+    
+    @IBAction func startStop(_ sender: Any) {
+    }
+    @IBAction func reset(_ sender: Any) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
 }
-
