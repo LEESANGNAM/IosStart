0027aac67d55d3d768c3f8984311ddd5fcf1ae40
LEESANGNAM <fgh0116@naver.com>
2021-11-09 02:28:17 +0900

    StopWatch_TimeInterval

--- StopWatch/StopWatch/ViewController.swift
+++ StopWatch/StopWatch/ViewController.swift
@@ -16,15 +16,45 @@ class ViewController: UIViewController {
     @IBOutlet weak var startStopButton: UIButton!
     @IBOutlet weak var resetButton: UIButton!
     
+    var isStarting = false
+    var timer = Timer()
     
+    var startTime = 0.0
+    var elapsed = 0.0
     
     @IBAction func startStop(_ sender: Any) {
+        if isStarting{
+            // stop
+            elapsed = Date().timeIntervalSince1970 - startTime
+            startStopButton.setTitle("Start", for: .normal)
+            timer.invalidate() //timer stop
+        }else{ // start
+            startTime = Date().timeIntervalSince1970 - elapsed
+            timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+            startStopButton.setTitle("Stop", for: .normal)
+        }
+        isStarting = !isStarting //반전
     }
     @IBAction func reset(_ sender: Any) {
+        labelMinute.text = "00"
+        labelSecond.text = "00"
+        labelSecond.text = "00"
     }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // 초기설정
+        
+        startStopButton.setTitle("Start", for: .normal)
+        resetButton.setTitle("Reset", for: .normal)
+        labelMinute.text = "00"
+        labelSecond.text = "00"
+        labelSecond.text = "00"
+    }
+    
+    @objc func updateCounter(){
+        
+        
     }
 
 }
