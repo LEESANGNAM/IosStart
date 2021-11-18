9b6a71d6cc1eb9115308ed686e6f95ca37378bfc
LEESANGNAM <fgh0116@naver.com>
2021-11-18 12:31:07 +0900

    SignIn_ViewDesign

---
+++ Leacture_signin/Leacture_signin/SignInViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SignInViewController.swift
+//  Leacture_signin
+//
+//  Created by 이상남 on 2021/11/18.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func dismissVC(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Leacture_signin/Leacture_signin/ViewController.swift
+++ Leacture_signin/Leacture_signin/ViewController.swift
@@ -14,6 +14,11 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func moveToSignIn(_ sender: Any) {
+        let signInVC = UIStoryboard(name: "SignInVC", bundle: nil).instantiateViewController(withIdentifier: "signInVC") // 스토리보드 아이디 지정
+        
+        self.present(signInVC, animated: true, completion: nil)
+    }
+    
 }
 
